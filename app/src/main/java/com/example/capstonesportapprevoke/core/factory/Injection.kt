cb1bc1d2d1ca5f0d57e4fb1d67e5a09a967a2d94
Marfin Fadhilah <marfin.fadhilah@bankneo.co.id>
2022-07-14 14:02:05 +0700

    implement certificate pinner protection

--- app/src/main/java/com/example/capstonesportapprevoke/core/factory/Injection.kt
+++ app/src/main/java/com/example/capstonesportapprevoke/core/factory/Injection.kt
@@ -10,6 +10,7 @@ import com.example.capstonesportapprevoke.core.domain.repository.ISportRepositor
 import com.example.capstonesportapprevoke.core.domain.usecase.SportInteractor
 import com.example.capstonesportapprevoke.core.domain.usecase.SportUseCase
 import com.example.capstonesportapprevoke.core.utils.AppExecutors
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import java.util.concurrent.TimeUnit
@@ -17,11 +18,17 @@ import java.util.concurrent.TimeUnit
 object Injection {
     private fun provideRepository(context: Context): ISportRepository {
         val database = SportDatabase.getInstance(context)
+        val hostname = "thesportsdb.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/Yl6IYMBnxrwtn9DWo32vSutvY3gRn56iEfWj5/66c88=")
+            .build()
+
 
         val okHttpClient = OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
         val remoteDataSource = RemoteDataSource.getInstance(ApiConfig.provideApiService(okHttpClient))
         val localDataSource = LocalDataSource.getInstance(database.sportDao())
