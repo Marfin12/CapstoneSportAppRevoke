197a52c3bb06503280765714c563c84932a66f08
Marfin Fadhilah <marfin.fadhilah@bankneo.co.id>
2022-07-19 11:24:30 +0700

    Apply split installing module ( need google play console account to test it)

--- app/src/main/java/com/example/capstonesportapprevoke/MainActivity.kt
+++ app/src/main/java/com/example/capstonesportapprevoke/MainActivity.kt
@@ -2,20 +2,26 @@ package com.example.capstonesportapprevoke
 
 import android.os.Build
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.capstonesportapprevoke.core.utils.FragmentUtils
+import com.example.capstonesportapprevoke.core.utils.FragmentUtils.goToNewFeatureFragment
 import com.example.capstonesportapprevoke.core.utils.FragmentUtils.handleBackStackNavigation
 import com.example.capstonesportapprevoke.databinding.ActivityMainBinding
+import com.example.capstonesportapprevoke.home.HomeFragment
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+import com.google.android.play.core.splitinstall.SplitInstallRequest
 import kotlinx.android.synthetic.main.main_fragment.*
 import kotlinx.android.synthetic.main.main_fragment.view.*
 import java.lang.Exception
@@ -45,16 +51,14 @@ class MainActivity : AppCompatActivity() {
             handleBackStackNavigation(navController)
             setupActionBarWithNavController(navController, drawerLayout)
             navigationView.setNavigationItemSelectedListener {
+                val homeFragment = HomeFragment()
                 when (it.itemId) {
                     R.id.newFeatureFragment -> {
                         try {
-                            NavigationUI.onNavDestinationSelected(it, navController)
-                            drawerLayout.closeDrawer(GravityCompat.START)
+                            goToNewFeatureFragment(drawerLayout, navController, it)
                             true
                         } catch (ex: Exception) {
-                            Toast.makeText(
-                                applicationContext,"Module not installed yet", Toast.LENGTH_SHORT
-                            ).show()
+                            installNewFeatureModule(drawerLayout, navController, it)
                             false
                         }
                     }
@@ -74,5 +78,30 @@ class MainActivity : AppCompatActivity() {
             navController,
             binding.drawerLayout
         )
+    }
+
+    private fun installNewFeatureModule(
+        drawerLayout: DrawerLayout, navController: NavController, menuItem: MenuItem
+    ): Boolean {
+        val splitInstallManager = SplitInstallManagerFactory.create(this)
+        val moduleChat = "newFeature"
+        if (splitInstallManager.installedModules.contains(moduleChat)) {
+            Toast.makeText(this, "Open module", Toast.LENGTH_SHORT).show()
+            goToNewFeatureFragment(drawerLayout, navController, menuItem)
+            return true
+        } else {
+            val request = SplitInstallRequest.newBuilder()
+                .addModule(moduleChat)
+                .build()
+            splitInstallManager.startInstall(request)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Success installing module", Toast.LENGTH_SHORT).show()
+                    goToNewFeatureFragment(drawerLayout, navController, menuItem)
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Error installing module", Toast.LENGTH_SHORT).show()
+                }
+            return false
+        }
     }
 }
--- app/src/main/java/com/example/capstonesportapprevoke/core/utils/FragmentUtils.kt
+++ app/src/main/java/com/example/capstonesportapprevoke/core/utils/FragmentUtils.kt
@@ -1,13 +1,22 @@
 package com.example.capstonesportapprevoke.core.utils
 
+import android.content.Context
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.capstonesportapprevoke.R
 import com.example.capstonesportapprevoke.core.domain.model.Team
 import com.example.capstonesportapprevoke.favorite.FavoriteFragment
 import com.example.capstonesportapprevoke.favorite.FavoriteFragmentDirections
 import com.example.capstonesportapprevoke.home.HomeFragment
 import com.example.capstonesportapprevoke.home.HomeFragmentDirections
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+import com.google.android.play.core.splitinstall.SplitInstallRequest
+import java.lang.Exception
 
 object FragmentUtils {
 
@@ -23,6 +32,14 @@ object FragmentUtils {
         isFromFavoriteFragment = false
         val action = HomeFragmentDirections.actionHomeFragmentToDetailActivity(teamData)
         findNavController(homeFragment).navigate(action)
+    }
+
+    fun goToNewFeatureFragment(
+        drawerLayout: DrawerLayout, navController: NavController, menuItem: MenuItem
+    ): Boolean {
+        NavigationUI.onNavDestinationSelected(menuItem, navController)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 
     fun handleBackStackNavigation(navController: NavController) {
